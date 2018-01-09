1bca5881d0332e5005cf33a5e8c4b89003870340
Shelby Banfield <SBanfield@ASOL.local>
2018-01-09 16:03:03 -0700

    Committing from work computer

--- src/main.go
+++ src/main.go
@@ -1,5 +1,6 @@
 /* Gonite main package.
 =============================
+*make an arg: gonite.exe -i jsonname to run custom json
 chatmansam.com/gonite
 This downloads the exe
 Exe runs, gets chatmansam.com/gonite/json.json to get updated json
