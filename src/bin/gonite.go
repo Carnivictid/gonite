2ac3b069abe161ab70d1a2ae4bc4403c482547b4
Quinten Becker <qbecker@asu.edu>
2018-01-10 21:22:13 -0700

    added concurrent downloding

--- src/bin/gonite.go
+++ src/bin/gonite.go
@@ -15,6 +15,12 @@ type Pkg struct {
     Flg string `json:"flg"`
 }
 
+
+//a little better practice than inline strings
+const exeDir string = "C:/temp/"
+const fExtension string = ".exe"
+
+
 func GetPkgsFromJson() []Pkg {
 	raw, err := ioutil.ReadFile("./bin/squires.json")
 	if err != nil {
@@ -25,24 +31,32 @@ func GetPkgsFromJson() []Pkg {
 	return c
 }
 
-func DownloadFile(filename string, url string) {
+//Here were going to pass it the package and a channel to report to once its downloaded.
+func DownloadFile(pack Pkg, downloads chan Pkg) {
+	//first time I ran it, crashed because the file path didn't exist.
+	//so let's check if dir exists, if not, create it.
+	if _, err := os.Stat(exeDir); os.IsNotExist(err) {
+    os.Mkdir(exeDir, 0777)
+  }
 	// create a blank file named "filename"
-	out, err := os.Create("C:/temp/" + filename + ".exe")
+	out, err := os.Create(exeDir + pack.Exe + fExtension)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer out.Close() // Close the file when we finish
-	
+
 	// Get the data from the url
-	resp, err := http.Get(url)
+	resp, err := http.Get(pack.Url)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer resp.Body.Close() // Close the response when we finish
-	
+
 	// Write the body to the file
 	_, err = io.Copy(out, resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
+	//send the package to downloads channel, indicating its done dowloading
+	downloads <- pack
 }
--- src/main.go
+++ src/main.go
@@ -13,12 +13,22 @@ func main() {
 	// pkgs is an array of structs
 	// Exe `json:"exe"`, Url `json:"url"`, Flg `json:"flg"`
 	pkgs := gonite.GetPkgsFromJson()
-	
+
+	//create a buffered channel the size of our json
+	downloads := make(chan gonite.Pkg, len(pkgs))
+
 	for _, p := range pkgs {
-		log.Printf("Installing: %v", p.Exe)
-		gonite.DownloadFile(p.Exe, p.Url)
-		RunExe(p.Exe, p.Flg)
-		log.Printf("%v has finished installing.", p.Exe)
+		log.Printf("Starting Download for: %v", p.Exe)
+		//spin off each one so downloads happen concurrently
+		go gonite.DownloadFile(p, downloads)
+	}
+
+	//As values come into the downloads channel, they start downloading.
+	//This blocks until a value is received, so installs happen
+	//in the order in which they finished downloading.
+	for i := range downloads{
+		RunExe(i.Exe, i.Flg)
+		log.Printf("%v has finished installing.", i.Exe)
 	}
 }
 
